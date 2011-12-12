54e0b342fdfc300eba510c23451dc919add7aed8
nes.manrique@gmail.com <nes.manrique@gmail.com@79b4bf3a-0acd-ef28-75ad-f7266902c8c6>
2011-12-12 01:03:09 +0000

    random data

--- trunk/captchaprep.cpp
+++ trunk/captchaprep.cpp
@@ -3,12 +3,44 @@
 #include <cv.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "captchaprep.h"
 
 using namespace cv;
+
+/* Noise reduction */
+
+Mat lessnoise(char * imagen){
+    Mat img = imread(imagen,CV_LOAD_IMAGE_GRAYSCALE);
+	Mat temp;
+
+	if(!img.data ){ continue; }
+	temp = img>128;
+
+	Mat element = getStructuringElement( MORPH_RECT, Size( 6, 6 )); 
+
+	/// Apply the dilation operation
+	dilate( temp, img, element );
+	erode( img, temp, element );
+
+    //Simple segmentation for 1 letter captcha
+    Rect roi(20,20,60,60);
+	resize(temp(roi), img,Size(40,40) ,0 , 0, INTER_NEAREST);
+
+    return img;
+	//imwrite(imagen,img);
+
+}
+
+/* Segmentation */
+
 
 int main(int argc,char* argv[]){
 
 	for(int i=1;i<argc;i++){
+
+        Mat img;
+        img=lessnoise(argv[i]);
+    /*
 		Mat img = imread(argv[i],CV_LOAD_IMAGE_GRAYSCALE);
 		Mat temp;
 
@@ -22,7 +54,8 @@ int main(int argc,char* argv[]){
 		erode( img, temp, element );
 
     	Rect roi(20,20,60,60);
-		resize(temp(roi), img,Size(40,40) ,0 , 0, INTER_NEAREST);
+		resize(temp(roi), img,Size(40,40) ,0 , 0, INTER_NEAREST);*/
+
 		imwrite(argv[i],img);
 	}
 	return 0;
